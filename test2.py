0c9c95b4069eabec74b119c69ce66428243ef572
Resn4R <purview-joust.0x@icloud.com>
2023-12-14 18:55:11 +0000

    testing pythong stuff, very basic

---
+++ test2.py
@@ -0,0 +1,18 @@
+print("Hello, World!")
+
+name = 'Python'
+name2 = "Python"
+print("hello, {name}".format(name=name))
+
+print(type(name2))
+
+x = 10
+y = 2
+
+print(x + y) # 12
+print(x - y) # 8
+print(x * y) # 20
+print(x / y) # 5.0
+print(x ** y) # 100
+print(x % y) # 0
+print(x // y) # 5
